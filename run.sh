00d642552d8396bc866a8920432e4d8a77d2b5dc
Doug Richardson <doug@rekt.email>
2020-02-22 00:10:59 -0800

    run against aws instance

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-ADDRESSES=(
-api.protonmail.ch
-34.65.23.66
-35.216.222.211
+declare -A ADDRESSES=(
+[api.protonmail.ch]='Proton Mail'
+[34.65.23.66]='GCP europe-west6-a (Zurich) premium network'
+[35.216.222.211]='GCP europe-west6-a (Zurich) standard network'
+[3.127.36.12]='AWS Frankfurt'
 )
 
-for addr in ${ADDRESSES[@]}; do
+for addr in ${!ADDRESSES[@]}; do
 	echo '#'
-	echo "# Testing $addr"
+	echo "# Testing $addr - ${ADDRESSES[$addr]}"
 	echo '#'
 	echo
 	echo "# traceroute $addr"
@@ -19,11 +20,11 @@ for addr in ${ADDRESSES[@]}; do
 	ping -c 10 -i 0.2 $addr
 	echo "ping exited with $?"
 	echo
-	echo "iperf3 --udp --omit 2 --bitrate 500M --client $addr"
-	iperf3 --udp --omit 2 --bitrate 500M --client $addr
+	echo iperf3 --connect-timeout 1000 --udp --omit 2 --bitrate 500M --client $addr
+	iperf3 --connect-timeout 1000 --udp --omit 2 --bitrate 500M --client $addr
 	echo "iperf3 exited with $?"
-	echo "iperf3 --udp --omit 2 --bitrate 500M --client $addr --reverse"
-	iperf3 --udp --omit 2 --bitrate 500M --client $addr --reverse
+	echo iperf3 --connect-timeout 1000 --udp --omit 2 --bitrate 500M --client $addr --reverse
+	iperf3 --connect-timeout 1000 --udp --omit 2 --bitrate 500M --client $addr --reverse
 	echo "iperf3 exited with $?"
 done
 
